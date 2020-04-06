4d0c3cebc93686bb1c410fda1e42e79c78f952e7
Francesco Cini <francesco.cini@gruppometa.it>
2020-04-06 19:02:45 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
+import { UserPanelComponent } from './components/user-panel/user-panel.component';
 
-const routes: Routes = [];
+const routes: Routes =  [
+  { path: 'user', component: UserPanelComponent },
+  { path: 'chat', component: ChatComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { UserPanelComponent } from './components/user-panel/user-panel.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPanelComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
