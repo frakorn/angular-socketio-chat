e74a840487c06ccdaed74d9cfdf1de0e343e186a
Francesco Cini <francesco.cini@gruppometa.it>
2020-04-06 19:26:43 +0200

    added some graphics

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  messages: { [key: string]: any }[];
+
   constructor() { }
 
   ngOnInit() {
+    this.messages = [{
+      name: 'George Clooney',
+      message: "The only failure is not to try"
+    }, {
+      name: 'Seth Rogen',
+      message: "I grew up in Vancouver, man. That's where more than half of my style comes from."
+    }, {
+      name: 'John Lydon',
+      message: "There's nothing glorious in dying. Anyone can do it."
+    }];
   }
 
 }
